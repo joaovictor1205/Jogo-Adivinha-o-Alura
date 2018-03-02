e8aae48f6ad4bc3e3b5ce085fbceae2b89ae851b
joao.goncalves <joaovictor.lol12@gmail.com>
2018-03-02 08:49:20 -0300

    Continuação do código

--- adivinhacao.py
+++ adivinhacao.py
@@ -2,29 +2,30 @@ print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
-numero_secreto = 42
+numero_secreto = 5
 total_tentavias = 3
-rodada = 1
 
-while(rodada <= total_tentavias):
+for rodada in range(1, total_tentavias + 1):
 
-    print("Tentativa:",rodada, "de", total_tentavias)
-    chute_str = input("Digite o seu número: ")
+    print("Tentativa {} de {}".format(rodada, total_tentavias))
+    chute_str = input("Digite um número entre 1 e 10: ")
     print("Você digitou :" ,chute_str)
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 10):
+        print("Valor não aceito!")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
 
-
     if(acertou):
         print("Parabéns! Você acertou!")
+        break;
     elif(maior):
         print("O seu chute foi maior do que o número secreto!")
     else:
         print("O seu chute foi menor do que o número secreto!")
-
-    rodada = rodada + 1
 
 print("*********************************")
 print("Fim do jogo")
