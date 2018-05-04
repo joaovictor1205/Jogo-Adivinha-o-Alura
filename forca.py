74616cdddccf8814a7b47c231230e0db5b6eb266
joao.goncalves <joaovictor.lol12@gmail.com>
2018-05-04 11:26:11 -0300

    Continuação do código

--- forca.py
+++ forca.py
@@ -1,9 +1,23 @@
 def jogar ():
 
     print("*********************************")
-    print("Bem vindo ao jogo de Adivinhação!")
+    print("Bem vindo ao jogo de Forca!")
     print("*********************************")
 
+    palavra_secreta = "palavra"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra?")
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrou a letra '{}' na posição {}".format(letra, index))
+            index = index + 1
 
     print("***********")
     print("Fim do jogo")
